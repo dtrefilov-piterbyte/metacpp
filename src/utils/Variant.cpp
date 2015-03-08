09d5248ffa666f44d480691e24046e5beeb4889b
alien <the-alien@live.ru>
2015-03-09 01:36:21 +0300

    Update Variant.cpp

--- src/utils/Variant.cpp
+++ src/utils/Variant.cpp
@@ -14,6 +14,7 @@
 * limitations under the License.                                            *
 ****************************************************************************/
 #include "Variant.h"
+#include <stdexcept>
 
 namespace metacpp
 {
